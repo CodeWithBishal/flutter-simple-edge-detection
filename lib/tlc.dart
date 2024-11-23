4abb8134a12838aca1458f7232f96ac2dae9f528
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2024-11-23 17:07:12 +0530

    Create tlc.dart

---
+++ lib/tlc.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+import 'dart:ffi';
+
+class TlcCalc {
+  final dylib = Platform.isAndroid
+      ? DynamicLibrary.open("libOpenCV_ffi.so")
+      : DynamicLibrary.process();
+  File calculateTLC(File imagePathtoCalc) {
+    final imagePath = imagePathtoCalc.path.toNativeUtf8();
+    final imageFfi = dylib.lookupFunction<Bool Function(Pointer<Utf8>),
+        bool Function(Pointer<Utf8>)>('detect_contour_tlc');
+    if (imageFfi(imagePath)) {
+      saveImage(imagePath.toDartString(), imagePathtoCalc.path);
+      return File(imagePath.toDartString());
+    } else {
+      return imagePathtoCalc;
+    }
+
+//ffi
+  }
+
+  void saveImage(String finalImage, filePath) async {
+    await File(filePath).writeAsBytes(File(finalImage).readAsBytesSync());
+  }
+}
